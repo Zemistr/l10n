a888e1266b5abb13f82d0b1fadf301207b4d9f0e
Zemistr <zemistr@gmail.com>
2015-03-15 19:14:20 +0100

    Added comments

--- src/l10n/Language/ILanguage.php
+++ src/l10n/Language/ILanguage.php
@@ -2,13 +2,28 @@
 namespace l10n\Language;
 
 interface ILanguage {
+	/**
+	 * @return string
+	 */
 	public function getIso639_1();
 
+	/**
+	 * @return string
+	 */
 	public function getIso639_2();
 
+	/**
+	 * @return string
+	 */
 	public function getEnglishName();
 
+	/**
+	 * @return string
+	 */
 	public function getOriginalName();
 
+	/**
+	 * @return bool
+	 */
 	public function isRtl();
 }
--- src/l10n/Plural/IPlural.php
+++ src/l10n/Plural/IPlural.php
@@ -2,9 +2,19 @@
 namespace l10n\Plural;
 
 interface IPlural {
+	/**
+	 * @param int $n
+	 * @return int
+	 */
 	public function getPlural($n = 1);
 
+	/**
+	 * @return string
+	 */
 	public function getPluralForm();
 
+	/**
+	 * @return int
+	 */
 	public function getPluralsCount();
 }
